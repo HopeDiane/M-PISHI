7d4544426831dcbc37d427323c1901b0ef24ecf7
stevenGithinji <mburusteven2@gmail.com>
2021-08-31 14:38:02 +0300

    Splash Commit

---
+++ app/src/main/java/com/example/mpishi/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.mpishi;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+
+
+
+    }
+
+}
