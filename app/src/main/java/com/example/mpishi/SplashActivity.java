8710adc9626cd79b4af57a1df91cef61dd0b91cf
stevenGithinji <mburusteven2@gmail.com>
2021-08-31 15:20:09 +0300

    Splash Commit

--- app/src/main/java/com/example/mpishi/SplashActivity.java
+++ app/src/main/java/com/example/mpishi/SplashActivity.java
@@ -1,11 +1,19 @@
 package com.example.mpishi;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    View first,second,third,fourth,fifth,sixth;
+    TextView mpishi,tag;
+
+    Animation topAnimation, bottomAnimation, middleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,29 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation);
+
+        first= findViewById(R.id.first_line);
+        second= findViewById(R.id.second_line);
+        third= findViewById(R.id.third_line);
+        fourth= findViewById(R.id.fourth_line);
+        fifth= findViewById(R.id.fifth_line);
+        sixth= findViewById(R.id.sixth_line);
+
+        mpishi= findViewById(R.id.mpishi);
+        tag=findViewById(R.id.tag);
+
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+
+        mpishi.setAnimation(topAnimation);
+        tag.setAnimation(topAnimation);
 
 
 
